701ae3e03e84c1b21948fabfec8ca8802ff7066f
Mateusz <mateusz.szymanczyk@iCloud.com>
2023-12-21 00:23:07 +0100

    poczatek konfiguracji react i fronta

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React, {useEffect, useState, useRef} from 'react';
+import Keycloak from 'keycloak-js';
+
+function App() {
+  const [keycloak, setKeycloak] = useState(null);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const isRun = useRef(false);
+
+  useEffect(() => {
+    if(isRun.current) return;
+    const keycloakInstance = new Keycloak({
+      url: 'http://127.0.0.1:8081',
+      realm: 'Travel-Agency',
+      clientId: 'React-FE-client'
+    });
+
+    isRun.current = true;
+
+    keycloakInstance.init({ onLoad: 'login-required'}).then(auth => {
+      setKeycloak(keycloakInstance);
+      setAuthenticated(auth);
+    });
+
+  }, []);
+
+  if(keycloak) {
+    if(authenticated) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              This is a React application secured by Keycloak.
+            </p>
+            {/* Render the rest of your app's components here */}
+          </header>
+        </div>
+      );
+    } else return (<div>Unable to authenticate!</div>);
+  }
+  return (
+    <div>Loading...</div>
+  );
+}
+
+export default App;
